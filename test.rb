2e631c31198d8bb5912c4c93722e0ec92cf0e317
Tomas Strachota <tstrachota@redhat.com>
2014-02-18 11:11:49 +0100

    log cropping

--- test.rb
+++ test.rb
@@ -19,13 +19,100 @@ class CommandResult
 end
 
 
+class LogCropper
+
+  def initialize(log_file, target_file, only_fail=false)
+    @log_file = log_file
+    @target_file = target_file
+    @only_fail = only_fail
+    clear_log
+    clear_target
+  end
+
+  def put_header
+    put_line
+    put_line "Tests started at: " + Time.now.strftime("%Y/%m/%d %H:%M:%S")
+    put_line
+  end
+
+  def log(command, command_no, result)
+    return if @only_fail and result.ok?
+    puts
+    put_line "command ##{command_no}"
+    puts command
+    put_line
+    puts get_log
+    puts
+    clear_log
+  end
+
+  protected
+
+  def put_line(text="")
+    text = " #{text} " unless text.empty?
+    length = 100 - text.length
+
+    line = "--#{text}" + "-"*length
+    puts line % text
+  end
+
+  def log_file_path
+    File.expand_path(@log_file)
+  end
+
+  def target_file_path
+    File.expand_path(@target_file)
+  end
+
+  def get_log
+    File.readlines(log_file_path)
+  end
+
+  def clear_log
+    File.open(log_file_path, 'w') do |f|
+    end
+  end
+
+  def clear_target
+    File.open(target_file_path, 'w') do |f|
+    end
+  end
+
+  def puts(*args)
+    File.open(target_file_path, "a") do |f|
+      f.puts(*args)
+    end
+  end
+
+end
+
+
+
 def indent_puts(str)
   indent = "   "*@current_section.size
-  
+
   str.split("\n").each do |line|
     puts indent+line.rstrip
   end
 end
+
+
+def loggers
+  time_prefix = Time.now.strftime("%Y%m%d_%H%M%S").to_s + "_"
+  time_prefix = ""
+
+  @loggers ||= [
+    LogCropper.new('~/.foreman/log/hammer.log', "./#{time_prefix}hammer.fail.log", true),
+    LogCropper.new('~/.foreman/log/hammer.log', "./#{time_prefix}hammer.log"),
+    LogCropper.new('~/foreman/log/development.log', "./#{time_prefix}foreman.fail.log", true),
+    LogCropper.new('~/foreman/log/development.log', "./#{time_prefix}foreman.log")
+  ]
+end
+
+loggers.each do |logger|
+  logger.put_header
+end
+
 
 def hammer(*args)
 
@@ -34,19 +121,25 @@ def hammer(*args)
 
   result = CommandResult.new
 
-  args.unshift("hammer")
+  original_args = args.clone
+  original_args.unshift("hammer")
+
+  #args.unshift("hammer")
+  args = ["scl", "enable", "ruby193", "cd /root/hammer/hammer-cli/; bundle exec './bin/hammer "+ args.join(" ") +"'"]
+
   Open3.popen3(*args) do |stdin, stdout, stderr, wait_thr|
     result.stdout = stdout.readlines.join("")
     result.stderr = stderr.readlines.join("")
     result.code = wait_thr.value.exitstatus.to_i
   end
 
-  if result.ok?
-    indent_puts args.join(" ")
-    #indent_puts result.stdout
-  else
-    indent_puts args.join(" ") + "    [command ##{@command_cnt}]".cyan
+  indent_puts original_args.join(" ") + "    [command ##{@command_cnt}]".cyan
+  unless result.ok?
     indent_puts result.stderr.rstrip.blue
+  end
+
+  loggers.each do |logger|
+    logger.log(original_args.join(" "), @command_cnt, result)
   end
 
   return result
@@ -147,9 +240,9 @@ section "architecture" do
     test "returns ok" do
       res.ok?
     end
-   
+
     test_has_columns out, "Id", "Name", "OS Ids"
-    
+
   end
 end
 
