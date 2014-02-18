836208b1e709141c83712d06c8f16c8fe2bd9c43
Tomas Strachota <tstrachota@redhat.com>
2014-02-18 11:29:19 +0100

    some basic tests

--- test.rb
+++ test.rb
@@ -163,6 +163,15 @@ def test(desc, &block)
   end
 end
 
+def simple_test(*args)
+  res = hammer *args
+  out = ListOutput.new(res.stdout)
+
+  test "returns ok" do
+    res.ok?
+  end
+end
+
 def test_has_columns(out, *column_names)
   column_names.each do |name|
     test "has column #{name}" do
@@ -220,6 +229,49 @@ end
 
 require 'pry'
 
+RAND = Random.rand(100).to_s
+org_name = "Org"+RAND
+os_name = "test_os"+RAND
+user = {
+  :login => "some_user"+RAND,
+  :mail => "some.user@email.com"
+}
+
+section "organization" do
+
+  section "create" do
+    res = hammer "organization", "create", "--name", org_name
+    out = ListOutput.new(res.stdout)
+
+    test "returns ok" do
+      res.ok?
+    end
+  end
+
+end
+
+section "user" do
+
+  section "create" do
+    simple_test "user", "create", "--login", user[:login], "--mail", user[:mail], "--password", "passwd", "--auth-source-id=1"
+  end
+
+  section "assing to organization" do
+    simple_test "organization", "add_user", "--name", org_name, "--user-id", "10"
+  end
+
+end
+
+
+section "operating system" do
+
+  section "create" do
+    simple_test "os", "create", "--name", os_name, "--major", '6'
+  end
+
+end
+
+
 section "architecture" do
 
   section "list" do
@@ -246,3 +298,10 @@ section "architecture" do
   end
 end
 
+section "deletions" do
+
+  section "organization" do
+    simple_test "organization", "delete", "--name", org_name
+  end
+
+end
