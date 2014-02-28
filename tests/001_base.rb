04aacc26176312555e03de628cd01821ed139a69
Tomas Strachota <tstrachota@redhat.com>
2014-02-28 21:36:37 +0000

    more tests

--- tests/001_base.rb
+++ tests/001_base.rb
@@ -20,6 +20,7 @@ section "user" do
     test_result res
   end
 
+  # FIX: assign by login
   section "assing to organization" do
     simple_test "organization", "add_user", "--name", @org[:name], "--user-id", @user_id
   end
@@ -42,14 +43,14 @@ section "architecture" do
     simple_test "architecture", "create", @arch
   end
 
-  section "info by id" do
+  section "info" do
     res = hammer "architecture", "info", @arch.slice(:name)
     out = ShowOutput.new(res.stdout)
 
     test_result res
 
-    test_has_columns out, "Id", "Name", "OS ids"
-
+    test_has_columns out, "Id", "Name", "OS id"
+    test_column_value out, "Name", @arch[:name]
   end
 end
 
@@ -60,6 +61,27 @@ section "partition table" do
     simple_test "partition_table", "create", @ptable
   end
 
+  section "info" do
+    res = hammer "partition_table", "info", @ptable.slice(:name)
+    out = ShowOutput.new(res.stdout)
+
+    test_result res
+
+    test_has_columns out, "Id", "Name", "OS id"
+    test_column_value out, "Name", @ptable[:name]
+    test_column_value out, "OS Family", @ptable[:os_family]
+  end
+
+  #FIX: template id not dumpable by name
+  section "dump" do
+    res = hammer "partition_table", "dump", @ptable.slice(:name)
+
+    test_result res
+
+    test "dumps the content" do
+      res.stdout.strip == "PARTITION LAYOUT"
+    end
+  end
 end
 
 
@@ -69,9 +91,25 @@ section "installation medium" do
     res = hammer "--csv", "medium", "create", @medium
     out = SimpleCsvOutput.new(res.stdout)
 
-    medium_id = out.column("Id")
+    @medium_id = out.column("Id")
 
     test_result res
+  end
+
+  section "info" do
+    res = hammer "medium", "info", @medium.slice(:name)
+    out = ShowOutput.new(res.stdout)
+
+    test_result res
+
+    test_has_columns out, "Id", "Path", "OS Family", "OS IDs", "OSs"
+    test_column_value out, "Name", @medium[:name]
+    test_column_value out, "OS Family", @medium[:os_family]
+    test_column_value out, "Path", @medium[:path]
+  end
+
+  section "assing to organization" do
+    simple_test "organization", "add_medium", "--name", @org[:name], "--medium", @medium[:name]
   end
 
 end
@@ -83,9 +121,24 @@ section "template" do
     res = hammer "--csv", "template", "create", @template
     out = SimpleCsvOutput.new(res.stdout)
 
-    template_id = out.column("Id")
+    @template_id = out.column("Id")
 
     test_result res
+  end
+
+  #FIX: template id not dumpable by name
+  section "dump" do
+    res = hammer "template", "dump", @template.slice(:name)
+
+    test_result res
+
+    test "dumps the content" do
+      res.stdout.strip == "TEMPLATE"
+    end
+  end
+
+  section "assing to organization" do
+    simple_test "organization", "add_configtemplate", "--name", @org[:name], "--configtemplate", @template[:name]
   end
 
 end
