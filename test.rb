3d1abfd7bc00f2302d83429c8100db7ff42fe485
Tomas Strachota <tstrachota@redhat.com>
2014-06-27 12:08:35 +0000

    usage description in the main test file

--- test.rb
+++ test.rb
@@ -6,6 +6,20 @@ require './lib.rb'
 require './output.rb'
 require './utils.rb'
 
+#
+# Available environment variables:
+#
+# HT_HAMMER_CMD       - path to the hammer command (default is hammer)
+# HT_TIMESTAMPED_LOGS - use timestamps in names of the output log files, set to 1 to enable the functionality (disabled by default)
+# HT_LOGS_LOCATION    - target location for the output logs (default is ./log/)
+# HT_FOREMAN_LOG_FILE - path to the Foreman's log (default is /var/log/foreman/development.log)
+# HT_HAMMER_LOG_FILE  - path to the Hammer's log (default is ~/.foreman/log/hammer.log)
+#
+# Example usage:
+#
+# HT_HAMMER_CMD=./hammer HT_FOREMAN_LOG_FILE=~/foreman/log/development.log ./test.rb ./tests/
+#
+
 
 # Find test files
 test_files = []
